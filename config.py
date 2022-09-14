96d10b1c3f4255dfa974cea21a34f86dcb0f9825
Дмитрий Редько <56727880+DmitryRedko@users.noreply.github.com>
2022-09-14 18:35:47 +0700

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-token = "vk1.a.ZYOY-rzWcwtANJVHxG3uMfQkFiW6x_fuvu9zRfMHV-dZU2KQv7hXoXGPu_HNl-3vOmR98UwbkVAYZnuQGLeWpB4RcZZoFX3VzstiKGD7iyvWmzkWrXXn_TC6Pc4OCaqsmuzbAh5NEfHH9U8tUGujkpu7e8IG4aKmsng1mQb6wyu3YVyWNAOUM5p3sSvu2TU4"
-passwordvk='dsa123'
-host = "127.0.0.1"
-user = "postgres"
-passwordbd = "5402"
-db_name = "postgres"
